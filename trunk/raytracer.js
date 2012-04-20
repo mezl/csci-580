c569e6e3acc9d04faaf71b2020880378570dd88f
jarvisliao@gmail.com <jarvisliao@gmail.com@29dbeb08-5096-ae0d-4e57-b1831c2348aa>
2012-04-20 06:19:20 +0000

    Sophia: upload again

--- trunk/raytracer.js
+++ trunk/raytracer.js
@@ -102,27 +102,92 @@ function Engine()
 					//@param {Primitive}
 					var light = p;
 					//@param {vector3}
-          var L = ((Sphere*)light.GetCentre() - pi;
-          NORMALIZE( L );
-          //@param {vector3}
-          var N = prim.GetNormal( pi );
-          if (prim.GetMaterial().GetDiffuse() > 0)
-          {
-            //@param {float}
-            var dot = DOT( N, L );
-            if (dot > 0)
-            {
-              //@param {float}
-              var diff = dot * prim.GetMaterial().GetDiffuse();
-              // add diffuse component to ray color
-              a_Acc += diff * prim.GetMaterial().GetColor() * light.GetMaterial().GetColor();
-            }
-          }
+					var L = light.GetCentre() - pi;
+					NORMALIZE( L );
+					//@param {vector3}
+					var N = prim.GetNormal( pi );
+					if (prim.GetMaterial().GetDiffuse() > 0)
+					{
+						//@param {float}
+						var dot = DOT( N, L );
+						if (dot > 0)
+						{
+						//@param {float}
+						var diff = dot * prim.GetMaterial().GetDiffuse();
+						// add diffuse component to ray color
+						a_Acc += diff * prim.GetMaterial().GetColor() * light.GetMaterial().GetColor();
+						}
+					}
 				}
 			}
 		}
 		return prim;
-  }
+	}
+	
+	this.InitRender = function()	
+	{
+		this.m_CurrLine = 20;
+		this.m_PPos = this.m_CurrLine * this.m_Width;
+		this.m_WX1 = -4, this.m_WX2 = 4, this.m_WY1 = this.m_SY = 3, this.m_WY2 = -3;
+		this.m_DX = (this.m_WX2 - this.m_WX1) / this.m_Width;
+		this.m_DY = (this.m_WY2 - this.m_WY1) / this.m_Height;
+		this.m_SY += this.m_CurrLine * this.m_DY;
+		this.m_LastRow = new Primitive*[this.m_Width];
+		memset( this.m_LastRow, 0, this.m_Width * 4 );
+	}
+	
+	this.Render = function()
+	{
+		//@param {vector3}
+		var o( 0, 0, -5 );
+		//@param {int}
+		var msecs = GetTickCount();
+		//@param {Primitive}
+		var lastprim = 0;
+		// render remaining lines
+		for ( var y = this.m_CurrLine; y < (this.m_Height - 20); y++ )
+		{
+			m_SX = m_WX1;
+			// render pixels for current line
+			for ( var x = 0; x < this.m_Width; x++ )
+			{
+				//@param {Color}
+				var acc( 0, 0, 0 );
+				//@param {vector3}
+				var dir = vector3( m_SX, m_SY, 0 ) - o;
+				NORMALIZE( dir );
+				//@param {Ray}
+				var r( o, dir );
+				//@param {float}
+				var dist;
+				//@param {Primitive}
+				var prim = Raytrace( r, acc, 1, 1.0, dist );
+				//@param {int}
+				var red = acc.r * 256;
+				var green = acc.g * 256;
+				var blue = acc.b * 256;
+			
+				if (red > 255) red = 255;
+				if (green > 255) green = 255;
+				if (blue > 255) blue = 255;
+				
+				var cc = new Color( red, green, blue);
+				////***////
+				setPixel(imageData, x, y, cc);
+				////***////
+				this.m_SX += this.m_DX;
+			}
+			this.m_SY += this.m_DY;
+			// see if we've been working to long already
+			if ((GetTickCount() - msecs) > 100) 
+			{
+				// return control to windows so the screen gets updated
+				this.m_CurrLine = y + 1;
+				return false;
+			}
+		}
+		return true;
+	}
 	//@param {float}
 	var m_WX1, m_WY1, m_WX2, m_WY2, m_DX, m_DY, m_SX, m_SY;
 	//@param {Scene}
