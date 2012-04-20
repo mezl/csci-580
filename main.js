8d414265ad710a5eda9fa2c75d6335c789b52a36
jarvisliao@gmail.com <jarvisliao@gmail.com@29dbeb08-5096-ae0d-4e57-b1831c2348aa>
2012-04-20 22:04:59 +0000

    update current ,need fix prototype scene problem

--- main.js
+++ main.js
@@ -16,22 +16,11 @@ function main()
 	var imageData = context.createImageData(canvas.width,canvas.height);
 
 
+  testRaytraceScene(imageData);
 	//Just draw line for test now
 	for(var i=0;i<256;i++)
 					setPixel(imageData,i,i,WHITE);
 
 	context.putImageData(imageData,0,0);
-}
-
-//#############################################################################
-// @param {image} imageData
-// @param {int} x,y
-// @return {Color} col
-function setPixel(imageData, x, y, col) {
-	index = (x + y * imageData.width) * 4;
-	imageData.data[index+0] = col.r;
-	imageData.data[index+1] = col.g;
-	imageData.data[index+2] = col.b;
-	imageData.data[index+3] = col.a;	
 }
 
