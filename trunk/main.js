505f3d467ee8d600e2ad08606f6e89a6f16bd659
jarvisliao@gmail.com <jarvisliao@gmail.com@29dbeb08-5096-ae0d-4e57-b1831c2348aa>
2012-04-25 23:37:27 +0000

    Kai: Add interface of effect and draw polygon

--- trunk/main.js
+++ trunk/main.js
@@ -10,9 +10,41 @@ var Engine;
 var context;
 var line = 0;
 var animateRequest;
+var totalTime = 0;
+//@param {Boolean}
+var USE_SHADOW = false;
+var USE_REFLECTION = false;
+var USE_REFRACTION = false;
+var USE_SPECULAR = false;
+var USE_POLYGON = false;
 //#############################################################################
 initMain();
-main()
+  animate();
+function toggleShadow(chkbox) { 
+    USE_SHADOW = (chkbox.checked);
+    cancelRequestAnimFrame(animateRequest);
+		redraw();
+}
+function toggleReflection(chkbox) { 
+    USE_REFLECTION = (chkbox.checked);
+    cancelRequestAnimFrame(animateRequest);
+		redraw();
+}
+function toggleRefraction(chkbox) { 
+    USE_REFRACTION= (chkbox.checked);
+    cancelRequestAnimFrame(animateRequest);
+		redraw();
+}
+function toggleSpecular(chkbox) { 
+    USE_SPECULAR= (chkbox.checked);
+    cancelRequestAnimFrame(animateRequest);
+		redraw();
+}
+function togglePolygon(chkbox) { 
+    USE_POLYGON = (chkbox.checked);
+    cancelRequestAnimFrame(animateRequest);
+		redraw();
+}
 function initMain()
 {
 	// Load the context of the canvas
@@ -23,14 +55,13 @@ function initMain()
 	context = canvas.getContext("2d");
 	imageData = context.createImageData(canvas.width,canvas.height);
 	Engine = new Engine();
-	Engine.SetTarget( 20, canvas.width, canvas.height);
-	Engine.InitRender( );
-
+	Engine.SetTarget( 20, canvasWidth, canvasHeight);
+	redraw();
 }
-
-function main()
+function redraw()
 {
-
+	totalTime = 0;
+	Engine.InitRender();
   //Draw a Gray background
   for(var i=0;i<canvasWidth;i++)
   {
@@ -39,9 +70,10 @@ function main()
       setPixel(imageData,i,j,GRAY);
     }
   }
-  animate();
+	animate();
+
 }
-var totalTime = 0;
+
 function animate(){
   var date = new Date();
 	var ms = date.getTime();
